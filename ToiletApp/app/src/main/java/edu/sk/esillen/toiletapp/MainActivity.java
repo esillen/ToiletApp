58f226e8aeeaecbf162c6f0d48b696bfa23e7737
Erik Sillén <erik.sillen@slagkryssaren.com>
2017-03-29 17:53:49 +0200

    wtf git

--- ToiletApp/app/src/main/java/edu/sk/esillen/toiletapp/MainActivity.java
+++ ToiletApp/app/src/main/java/edu/sk/esillen/toiletapp/MainActivity.java
@@ -5,13 +5,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class CheckoutActivity extends AppCompatActivity {
+import com.orm.SchemaGenerator;
+import com.orm.SugarContext;
+import com.orm.SugarDb;
+
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_checkout);
+        setContentView(R.layout.activity_main);
+        /*
+        SugarContext.terminate();
+        SchemaGenerator schemaGenerator = new SchemaGenerator(getApplicationContext());
+        schemaGenerator.deleteTables(new SugarDb(getApplicationContext()).getDB());
+        SugarContext.init(getApplicationContext());
+        schemaGenerator.createDatabase(new SugarDb(getApplicationContext()).getDB());
+        */
     }
+
 
     public void checkin(View view){
         Intent intent = new Intent(this, PoopingActivity.class);
@@ -22,6 +34,4 @@ public class CheckoutActivity extends AppCompatActivity {
         Intent intent = new Intent(this, GraphsActivity.class);
         startActivity(intent);
     }
-
-
 }
